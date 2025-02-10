ada30f651b563a9c10406c19aaf2483a5ff021f6
Gryhon <76682050+Gryhon@users.noreply.github.com>
2025-02-10 15:36:15 +0100

    move artillery calculator and show a mil table ingame (#12)(#4)

--- rcon/discord/comfort.py
+++ rcon/discord/comfort.py
@@ -56,10 +56,12 @@ class Comfort (commands.Cog, DiscordBase):
         except Exception as e:
             logger.error(f"Unexpected error: {e}")
 
+    '''
     async def calculate_Mils (self, distance, min_distance, max_distance, min_mils, max_mils):
         mil = (((distance - min_distance) / (max_distance - min_distance)) * (min_mils - max_mils) + max_mils)
         logger.debug (f"Calculated Mil value for distance {distance} = {mil}")
         return round (mil)
+    '''
 
     async def background_task(self):
         while not self.shutdown_event.is_set():
@@ -107,92 +109,6 @@ class Comfort (commands.Cog, DiscordBase):
      
         logger.info(f"Broadcast message by {interaction.user.name} to {fraction_value} {'fractions' if fraction_value == 'both' else ''}: {message.replace("\n", "")}")
         await interaction.response.send_message(f"Broadcast message to {fraction_value} {'fractions' if fraction_value == 'both' else ''}:\n{message}")        
-
-    @app_commands.command(name="mil_calculator", description="Calculate mils from distance in meters")
-    @app_commands.describe(fraction="Choose a fraction")
-    @app_commands.choices(fraction=[
-        app_commands.Choice(name="Germany", value="DE"),
-        app_commands.Choice(name="USA", value="US"),
-        app_commands.Choice(name="USSR", value="USSR"),
-        app_commands.Choice(name="England", value="GB"),
-    ])
-    async def mil_calculator(self, interaction: discord.Interaction, fraction: app_commands.Choice[str]):
-        logger.info(f"Mil Calculator used by {interaction.user.name} for fraction {fraction.name}.")
-
-        # Check if the interaction occurs in a text channel
-        if not isinstance(interaction.channel, discord.TextChannel):
-            await interaction.response.send_message("Threads can only be created in text channels.", ephemeral=True)
-            return
-
-        await interaction.response.send_message(
-            "A private thread has been created for your calculation. Please enter the distance in meters.\n"
-            "Type `exit` to leave the calculator.",
-            ephemeral=True
-        )
-
-        # Create a private thread
-        private_thread = await interaction.channel.create_thread(
-            name=f"{fraction.name} Mil Calculator - {interaction.user}",
-            type=discord.ChannelType.private_thread,
-            auto_archive_duration=60 # 1440
-        )
-        await private_thread.add_user(interaction.user)
-        await private_thread.send(f"Welcome to your exlusive Mil Calcuator, {interaction.user.mention}!\n\n"
-                                   "The thread will be automatically deleted if you enter `exit` or \n"
-                                   "after 10 minutes of inactivity.\n\n"
-                                   "Valid input values are between 100 and 1600 meters.")
-
-        def check(msg):
-            return msg.author == interaction.user and msg.channel == private_thread
-
-        while True:
-            try:
-                # Wait for user input in the thread
-                message = await self.bot.wait_for('message', timeout=600.0, check=check)
-
-                if message.content.lower() == "exit":
-                    await private_thread.send("Exiting the calculator. See you next time!")
-                    await asyncio.sleep (2) 
-                    break
-
-                try:
-                    mil_value = None
-
-                    # Parse distance and calculate Mils
-                    distance = int(message.content)
-
-                    if distance >= 100 and distance <= 1600:
-                        if fraction.value == "DE":
-                            mil_value = await self.calculate_Mils(distance, 100, 1600, 622, 978)
-
-                        elif fraction.value == "US":
-                            mil_value = await self.calculate_Mils(distance, 100, 1600, 622, 978)
-
-                        elif fraction.value == "USSR":
-                            mil_value = await self.calculate_Mils(distance, 100, 1600, 800, 1120)
-
-                        elif fraction.value == "GB":
-                            if (distance >= 200 and distance <= 800) or (distance >= 1100 and distance <= 1200): 
-                                dis = distance - 5
-                            else:
-                                dis = distance
-
-                            mil_value = await self.calculate_Mils(dis, 100, 1600, 267, 533)
-
-                        await private_thread.send(f"Calculated Mil at {distance} meters: {mil_value} Mil.")
-                    else:
-                        await private_thread.send(f"Distance must be between 100 and 1600 meter!")
-
-                except ValueError:
-                    await private_thread.send("Invalid input. Please enter a valid number for the distance.")
-
-            except asyncio.TimeoutError:
-                await private_thread.send("Yue are inactive since 10 minutes. The thread will be closed now.")
-                await asyncio.sleep (5) 
-                break
-
-        await private_thread.delete(reason="Thread cleanup")
-        logger.info(f"Close Mil Calculator used by {interaction.user.name} for fraction {fraction.name}.")
 
     @commands.Cog.listener()
     async def on_ready(self):
