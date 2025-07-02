96149cb7a910de4b35cc95d11123e95e45c23912
Gryhon <76682050+Gryhon@users.noreply.github.com>
2025-07-02 10:57:28 +0200

    Remove voter registration (#33)

--- rcon/discord/registration_namechange/__init__.py
+++
@@ -1,7 +0,0 @@
-from .registration import Registration
-from .namechange import NameChange
-from .unregister import Unregister
-from .update_name import UpdateName
-from .name_emoji import NameEmoji
-
-__all__ = ['Registration', 'NameChange', 'Unregister', 'UpdateName', 'NameEmoji'] 
