5216a70cd37063f01b484642719d1c6ffe2c5306
Gryhon <76682050+Gryhon@users.noreply.github.com>
2025-11-05 22:51:08 +0100

    API changes due to U18 (#21)

--- rcon/discord/punishme.py
+++ rcon/discord/punishme.py
@@ -40,12 +40,12 @@ class PunishMe (commands.Cog, DiscordBase):
 
                     if ingame.is_Player_Ingame (player_id) == True:
                          
-                        data = {"player_name": str(ingame.get_Ingame_Player_Name (player_id)), "reason": "Because you said so! (Respawn)"}
+                        data = {"player_id": str (player_id), "player_name": str(ingame.get_Ingame_Player_Name (player_id)), "reason": "Because you said so! (Respawn)"}
 
                         await rcon.Punish_Player(data)
                         await safe_send(interaction, "✅ done")
 
-                        logger.info(f"Punish player {player_id} ({data['player_name']}) by {interaction.user.name}.")
+                        logger.info(f"Punish player {player_id} ({data['player_id']}) by {interaction.user.name}.")
                     else:
                         await safe_send(interaction, "❌ You are not ingame. Please join a server first.")
                         logger.info(f"{interaction.user.name} is not ingame. Cannot execute punish_me command.")
